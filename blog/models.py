f6843b6be671f835c2c558a4ca3008782df18298
nkleee <nogi3001@gmail.com>
2023-07-26 10:55:20 +0900

    유저 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +8,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
