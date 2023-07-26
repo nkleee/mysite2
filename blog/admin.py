ad0054025a1439c117e5260ed2cbc042d37d1b0a
nkleee <nogi3001@gmail.com>
2023-07-26 11:42:06 +0900

    URL 경로 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
